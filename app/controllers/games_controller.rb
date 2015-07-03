1d2cc9b943249ce66018bb0979bd5deca83e9d41
Rick Runyon <rick.runyon@gmail.com>
2015-07-03 09:30:18 -0400

    new homepage for picking pitch/45s

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,10 @@ class GamesController < ApplicationController
 
   def new
   end
+
+  def pitch
+  end
+
+  def fortyfives
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'games#new'
+
+  get 'pitch', to: 'games#pitch'
+  get 'fortyfives', to: 'games#fortyfives'
 end
